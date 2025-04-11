e6a25103a8aeb7962c99014a404d724677588984
alniv94 <alvinmiracastillo@gmail.com>
2025-04-12 07:37:18 +0800

    asd

--- resources/js/pages/residence1/srimage.vue
+++
@@ -1,7 +0,0 @@
-<template>
-    
-<!-- Additional Image Below the Table -->
-<div class="w-full">
-  <img src="../../images/layout2.jpg" alt="Additional Information" class="w-full">
-</div>
-</template>
